a71b15a2d15e335b995de6aa7015b834584d236f
Pupocka <d.melnitsa@yandex.ru>
2024-08-30 13:11:04 +0300

    open sidebar

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
 
 export const ArticleParamsForm = () => {
+	const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+	const handleToggleSidebar = () => {
+		setIsSidebarOpen(prevState => !prevState);
+	};
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton onClick={handleToggleSidebar} isOpened={isSidebarOpen}  />
+			<aside 
+				className={clsx(styles.container, isSidebarOpen ? styles.container_open : '')}
+			>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
